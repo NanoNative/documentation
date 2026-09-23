7235af012b26a288a0fe948d555bae645c4489c9
ab <22692390+absketches@users.noreply.github.com>
2026-09-23 11:00:09 +0200

    blog-ROP

--- content/blog/posts.ts
+++ content/blog/posts.ts
@@ -3,9 +3,11 @@ import changingRuntimeConfig from "./changing-nano-configuration-at-runtime";
 import orchestrationToChoreography from "./from-orchestration-to-choreography";
 import nativeJavaServices from "./why-nano-fits-native-java-services";
 import railixPlayingAGame from "./what-if-building-systems-felt-like-playing-a-game";
+import railwayApplicationPath from "./railway-oriented-programming";
 import type { BlogPost } from "./types";
 
 export const blogPosts: BlogPost[] = [
+    railwayApplicationPath,
     railixPlayingAGame,
     orchestrationToChoreography,
     changingRuntimeConfig,
